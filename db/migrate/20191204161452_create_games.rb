b093b4db1ee328c0753b16905aa62190b6a563d2
Jonathan Norman <jnorman2123@gmail.com>
2019-12-04 16:24:38 +0000

    created games and consoles migrations

--- db/migrate/20191204161452_create_games.rb
+++ db/migrate/20191204161452_create_games.rb
@@ -1,4 +1,10 @@
 class CreateGames < ActiveRecord::Migration
   def change
+    create_table :games do |t|
+      t.string :name
+      t.decimal :price
+      t.interger :console_id
+      t.integer :user_id
+    end
   end
 end
--- db/migrate/20191204161501_create_consoles.rb
+++ db/migrate/20191204161501_create_consoles.rb
@@ -1,4 +1,8 @@
 class CreateConsoles < ActiveRecord::Migration
   def change
+    create_table :consoles do |t|
+      t.string :name
+      t.integer :user_id
+    end
   end
 end

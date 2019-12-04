fcd1932e8aa852d8619c139908bd6112ab5bf8f2
Jonathan Norman <jnorman2123@gmail.com>
2019-12-04 17:16:49 +0000

    made helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,20 @@ class ApplicationController < Sinatra::Base
     erb :homepage
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      current_user ||= User.find(session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
+    end
+  end
+
 end

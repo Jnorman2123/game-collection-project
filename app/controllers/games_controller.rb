f12d9e1a4cc61b5a03d99f0e9ce4a60d42678777
Jonathan Norman <jnorman2123@gmail.com>
2019-12-06 14:55:03 +0000

    fixed bugs with owned games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,8 +11,8 @@ class GamesController < ApplicationController
   post '/games/wishlist' do
     redirect_if_not_logged_in
     game = Game.new(params)
-    binding.pry
     if !game.name.empty? && !game.price.empty?
+      game.user_id = current_user.id
       game.save
       redirect "/games/wishlist"
     else
@@ -25,21 +25,22 @@ class GamesController < ApplicationController
     redirect_if_not_logged_in
     @user = current_user
     @consoles = @user.consoles
-    @games = Game.all
+    @games = @user.games
     erb :"/games/owned"
   end
 
   post '/games/owned' do
     redirect_if_not_logged_in
     if game = Game.find_by_id(params[:game_id])
-      game.user_id = current_user.id
       game.console_id = params[:console_id]
+      game.owned = true
       game.save
       redirect "/games/owned"
     else
       if !params[:name].empty?
-        new_game = Game.create(params)
+        new_game = Game.new(params)
         new_game.user_id = current_user.id
+        new_game.owned = true
         new_game.save
         redirect "/games/owned"
       else
@@ -59,7 +60,7 @@ class GamesController < ApplicationController
   delete '/games/:id' do
     redirect_if_not_logged_in
     @game = Game.find_by_id(params[:id])
-    if @game.user_id != nil
+    if @game.owned == true
       @game.delete
       redirect '/games/owned'
     else
